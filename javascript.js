e6fc3164e4c036e8e429f2ce971c0e06e097ae6c
Kiryl21 <kirillevst1@gmail.com>
2024-09-02 23:13:26 +0200

    Add getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
 console.log("Hello World")
+
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random()*3);
+    if (computerChoice === 0) {
+        computerChoiceString = "rock";
+    } else if (computerChoice === 1) {
+        computerChoiceString = "paper";
+    } else {
+        computerChoiceString = "scissors";
+    }
+    return console.log(computerChoiceString);
+}
