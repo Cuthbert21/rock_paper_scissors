3257c75d1616cee1fcd733b56961e1d4205d6b12
Kiryl21 <kirillevst1@gmail.com>
2024-09-03 14:15:21 +0200

    Add playRound function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
 console.log("Hello World")
 
+humanScore = 0;
+
+computerScore = 0;
+
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random()*3);
     if (computerChoice === 0) {
@@ -9,10 +13,38 @@ function getComputerChoice() {
     } else {
         computerChoiceString = "scissors";
     }
-    return console.log(computerChoiceString);
+    console.log("Computer Choice is " + computerChoiceString)
+    return computerChoiceString;
 }
 
 function getHumanChoice() {
     let humanChoice = prompt("Make a choice!").toLowerCase();
-    return console.log(humanChoice)
+    console.log("Human Choice is " + humanChoice)
+    return humanChoice
+}
+
+function playRound() {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    if (humanSelection === computerSelection) {
+        console.log("Draw")
+    } else if (humanSelection === "paper" && computerSelection === "rock") {
+        console.log("You win! Paper beats Rock");
+        humanScore++;
+    } else if (humanSelection === "rock" && computerSelection === "scissors") {
+        console.log("You win! Rock beats Scissors");
+        humanScore++;
+    } else if (humanSelection === "scissors" && computerSelection === "paper") {
+        console.log("You win! Scissors beats Paper");
+        humanScore++;
+    } else if (computerSelection === "paper" && humanSelection === "rock") {
+        console.log("You lose! Paper beats Rock");
+        computerScore++;
+    } else if (computerSelection === "rock" && humanSelection === "scissors") {
+        console.log("You lose! Rock beats Scissors");
+        computerScore++;
+    } else if (computerSelection === "scissors" && humanSelection === "paper") {
+        console.log("You lose! Scissors beats Paper");
+        computerScore++;
+    } 
 }
